657288566316e01c8586080cd32ffd1e4e06e951
Gautam Mehta <gautam@giffgaff.co.uk>
2020-01-11 18:48:56 +0000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+#
+# multi stage docker file
+#
+# phase 1 - the build phase
+#
+
+FROM node:alpine as builder
+WORKDIR '/app'
+COPY package.json .
+RUN npm install
+COPY . .
+RUN npm run build
+
+#
+# output of the above will be
+# /app/build <-- all the stuff we care about is in this folder
+#
+
+# phase 2 - the run phase
+
+FROM nginx
+COPY --from=builder /app/build /usr/share/nginx/html
+
---
+++ build/precache-manifest.9def974e27fdd78653dc1b9f76e096f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c199598af333ad9809b9ffb121561be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1acfd4371d8b325d86e3",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a474521bb7a8b8cfd1c3",
+    "url": "/static/js/2.f88ab5aa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f88ab5aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1acfd4371d8b325d86e3",
+    "url": "/static/js/main.5f1e4981.chunk.js"
+  },
+  {
+    "revision": "b5ff61001688404db26f",
+    "url": "/static/js/runtime-main.8c5f780f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
